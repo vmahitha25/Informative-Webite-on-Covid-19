f14752a154e2d225a2489e6a6cc390d45a4ced31
pavani <saipavani2119@gmail.com>
2022-04-11 23:02:59 +0530

    Add to Homescreen

--- sw.js
+++ sw.js
@@ -15,13 +15,12 @@ var urlsToCache = [
 ];
 self.addEventListener('install', function (event){
     event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(function (cache){
+        caches.open(CACHE_NAME).then(function (cache){
             console.log('Opened cache');
             return cache.addAll(urlsToCache);
         })
     )
-});
+})
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(
